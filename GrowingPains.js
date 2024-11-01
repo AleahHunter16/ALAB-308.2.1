c545c5028cec91b0d419f612050aa58a519450dc
AleahHunter16 <AleahHunter16@gmail.com>
2024-11-01 19:21:40 -0400

    changes

--- GrowingPains.js
+++ GrowingPains.js
@@ -1,14 +1,13 @@
 const PI = 3.1415;
-const radius= 5;
-const area= PI*radius
-const ReqSpaceOfPlant=0.8;
-const AmtOfPlants=20;
+const radius = 5;
+const area = PI * Math.pow(radius, 2); // Area of circle
+const ReqSpaceOfPlant = 0.8;
+const AmtOfPlants = 20;
 
-const MaxNumberPlants=Math.floor(area/ReqSpaceOfPlant)
-console.log (MaxNumberPlants$ {MaxNumberPlants},Plants);
+const MaxNumberPlants = Math.floor(area / ReqSpaceOfPlant);
+console.log(`Max Number of Plants: ${MaxNumberPlants}`);
 
 function Growth(Weeks) {
-    let Plants=AmtOfPlants*Math.pow(2,Weeks);
-    console.log('AmtOfPlants ${weeks}week(s): $')
+    let Plants = AmtOfPlants * Math.pow(2, Weeks);
+    console.log(`Amount of Plants after ${Weeks} week(s): ${Plants}`);
 }
-
